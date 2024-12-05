6542454703cd5406b0601848f305e537968fea02
Gabriela Riedel <gabrielariedel1@gmail.com>
2024-12-04 22:38:08 -0300

    add: refinamento index.js (funcionou de primeira kakaka)

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -4,141 +4,43 @@ config.scene = {
   update: update,
 }
 
-let game = new Phaser.Game(config);
+class Game extends Phaser.Game {
+  constructor () {
+    super(config)
 
-let platforms;
-let score = 0;
-let scoreText;
-let player;
+    // this.audio = document.querySelector('audio')
 
-function preload() {
-  this.load.image("sky", "assets/images/sky.png");
-  this.load.image("ground", "assets/images/platform.png");
-  this.load.image("star", "assets/images/star.png");
-  this.load.image("bomb", "assets/images/bomb.png");
-  this.load.spritesheet("dude", "assets/images/dude.png", {
-      frameWidth: 32,
-      frameHeight: 48,
-  });
-}
+    // let iceServers
+    // if (window.location.host === 'feira-de-jogos.dev.br') {
+    //   this.socket = io({ path: '/adcieqipt20241-vitorhlmm-Jogo/socket.io/' })
+    //   iceServers = [
+    //     {
+    //       urls: 'turn:feira-de-jogos.dev.br',
+    //       username: USENARME,
+    //       credential: CREDENTIAL
+    //     }
+    //   ]
+    // } else {
+    //   this.socket = io()
+    //   iceServers = [
+    //     {
+    //       urls: 'stun:stun.l.google.com:19302'
+    //     }
+    //   ]
+    // }
+    // this.iceServers = { iceServers }
 
-function create() {
-  this.add.image(400, 300, "sky");
+    // this.socket.on('connect', () => {
+    //   console.log('Conectado ao servidor!')
+    // })
 
-  platforms = this.physics.add.staticGroup();
+    // Inclua novas cenas aqui
+    this.scene.add('cenaDeAmostra', cenaDeAmostra)
 
-  platforms.create(400, 568, "ground").setScale(2).refreshBody();
-
-  platforms.create(600, 400, "ground");
-  platforms.create(50, 250, "ground");
-  platforms.create(750, 220, "ground");
-
-  
-
-  player = this.physics.add.sprite(100, 450, "dude");
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-  });
-
-  this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
-      frameRate: 20,
-  });
-
-  this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-  });
-
-  this.physics.add.collider(player, platforms);
-
-  stars = this.physics.add.group({
-      key: "star",
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-  });
-
-  stars.children.iterate(function(child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
-
-  this.physics.add.collider(stars, platforms);
-
-  this.physics.add.overlap(player, stars, collectStar, null, this);
-
-  function collectStar(player, star) {
-      star.disableBody(true, true);
-
-      score += 10;
-      scoreText.setText("Score: " + score);
-
-      if (stars.countActive(true) === 0) {
-          stars.children.iterate(function(child) {
-              child.enableBody(true, child.x, 0, true, true);
-          });
-
-          let x =
-              player.x < 400
-                  ? Phaser.Math.Between(400, 800)
-                  : Phaser.Math.Between(0, 400);
-
-          let bomb = bombs.create(x, 16, "bomb");
-          bomb.setBounce(1);
-          bomb.setCollideWorldBounds(true);
-          bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-      }
-  }
-
-  scoreText = this.add.text(0, -200, "Score: 0", {
-      fontSize: "32px",
-      fill: "#000",
-  });
-
-  bombs = this.physics.add.group();
-
-  this.physics.add.collider(bombs, platforms);
-
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
-
-  function hitBomb(player, bomb) {
-      this.physics.pause();
-
-      player.setTint(0xff0000);
-
-      player.anims.play("turn");
-
-      gameOver = true;
   }
 }
 
-function update() {
-  let cursors = this.input.keyboard.createCursorKeys();
-
-  if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-
-      player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-
-      player.anims.play("right", true);
-  } else {
-      player.setVelocityX(0);
-
-      player.anims.play("turn");
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
-  }
+window.onload = () => {
+  window.game = new Game()
 }
+
